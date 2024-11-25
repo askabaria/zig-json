29ba15b958731b6e81d454fc3a827d569a35bd14
Askabaria <niklas.becker.dev@gmail.com>
2024-11-25 17:21:56 +0100

    fix: build script

--- build.zig
+++ build.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-    const module = b.addModule("zig-json", .{ .root_source_file = .{ .path = "src/main.zig" } });
+    const module = b.addModule("zig-json", .{ .root_source_file = b.path("src/main.zig") });
     defer _ = module;
 
     const lib = b.addStaticLibrary(.{
